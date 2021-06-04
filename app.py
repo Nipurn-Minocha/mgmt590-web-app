20e31bd73aab8b779df47a1270b4f78f34f15cc6
Nipurn-Minocha <54888893+Nipurn-Minocha@users.noreply.github.com>
2021-06-03 23:29:40 -0400

    Update app.py
    code updated

--- app.py
+++ app.py
@@ -9,46 +9,47 @@ import time
 
 url = "{}".format(os.environ.get('PG_API_URL'))
 
-st.title('Amazing Question Answering Thing!')
+st.title('Question answering app !!')
 
-if st.button('Currently available models!'):
+if st.button('Click to view available models'):
     newurl=url + 'models'
     response=requests.request("GET",newurl)
     models=response.json()
     for m in models:
         st.success(m['name'])
 
+st.title('Add a model')
+name=st.text_input('name')
+tokenizer= st.text_input('tokenizer')
+model=st.text_area('model')
 
-if st.title('Add a model'):
-    name=st.text_input('name')
-    tokenizer= st.text_input('tokenizer')
-    model=st.text_area('model')
-    if st.button('Hit to add'):
-        newurl=url + 'models'
-        payload=json.dumps({
-            "name":name,
-            "tokenizer":tokenizer,
-            "model":model
-        })
-        headers = {'Content-Type': 'application/json'}
-        response = requests.request("PUT", newurl, headers=headers, data=payload)
-        st.success("Successfully added new model '{}'".format(response.json()[-1]['name']))
+if st.button('Enter to add model'):   
+    newurl=url + 'models'
+    payload=json.dumps({
+    "name":name,
+    "tokenizer":tokenizer,
+    "model":model
+    })
+    headers = {'Content-Type': 'application/json'}
+    response = requests.request("PUT", newurl, headers=headers, data=payload)
+    st.success("Successfully added new model '{}'".format(response.json()[-1]['name']))
 
-if st.title('Delete a model'):
-    allmodels=url + 'models'
-    response=requests.request("GET",allmodels)
-    models=response.json()
-    modellist=[]
-    for m in models:
-        modellist.append(m['name'])
-    option=st.selectbox('Select a model',modellist)
-    st.write('You have selected:',option)
-    if st.button('Are you sure you want to delete the selected model'):
-        updatedurl=url + 'models' + '?model=' + option
-        response1=requests.request("DELETE",updatedurl)
-        st.success("Successfully deleted model '{}'".format(option))
 
-# Inputs
+st.title('Delete Models')
+allmodels=url + 'models'
+response=requests.request("GET",allmodels)
+models=response.json()
+modellist=[]
+for m in models:
+    modellist.append(m['name'])
+option=st.selectbox('Select a model',modellist)
+if st.button('Delete Model'):
+    updatedurl=url + '/models' + '?model=' + option
+    response1=requests.request("DELETE",updatedurl)
+    st.success("Successfully deleted model '{}'".format(option))
+    
+
+st.title('Answer the Question')
 question = st.text_input('Question')
 context = st.text_area('Context')
 
@@ -65,8 +66,10 @@ if st.button('Answer Question'):
 
     st.success(answer)
 
-uploaded_file = st.file_uploader("Choose a file to upload")
+st.title("Upload excel with questions and contexts")
 
+uploaded_file = st.file_uploader("Choose a file to upload")
+    
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     answer_list=[]
@@ -81,8 +84,7 @@ if uploaded_file is not None:
         headers = {'Content-Type': 'application/json'}
         response = requests.request("POST", newurl, headers=headers, data=payload)
         answer = response.json()['answer']
-        row['finalanswer'] = answer
         answer_list.append(answer)
         time.sleep(0.5)
     df['finalanswer']=answer_list
-    st.table(df)      
+    st.table(df)     
